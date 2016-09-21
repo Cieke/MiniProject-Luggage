ccbd351230ed2d43f3971101c26e4c018bc93c0e
Kevin Van Robbroeck <kevin.van.robbroeck@gmail.com>
2016-09-21 11:00:24 +0200

    Switched over to application.properties driven JPA configuration (e.g. let Spring Boot do the hard work).
    Also added a physical naming strategy to remain compatible with previous database naming.

---
+++ src/test/java/com/realdolmen/spring/blog/ItemPersistenceTest.java
@@ -0,0 +1,30 @@
+package com.realdolmen.spring.blog;
+
+import com.realdolmen.spring.blog.domain.Item;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = ApplicationConfiguration.class)
+@Transactional
+public class ItemPersistenceTest {
+    @PersistenceContext
+    private EntityManager em;
+
+    @Test
+    public void itemCanBePersisted() throws Exception {
+        Item item = new Item();
+        item.setItemColor("red");
+        item.setItemName("Baloon");
+        em.persist(item);
+        Assert.assertNotNull(item.getId());
+    }
+}
